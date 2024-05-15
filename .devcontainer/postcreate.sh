bb56c8472af1ab371925e23df6e08bcf2111ac8a
Germain-L <germain.leignel@gmail.com>
2024-05-15 12:07:30 +0000

    chore: Update devcontainer.json to remove Docker Compose configuration and use a postCreateCommand script

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# start postgres
+service postgresql start
+
+# create user on postgres db
+psql -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+psql -U postgres -c "ALTER USER $POSTGRES_USER WITH SUPERUSER;"
+psql -U postgres -c "CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;"
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;"
+
+# Install cosmtrek/air
+go install github.com/cosmtrek/air@latest
